c01d82db4466895f00e4dfd8d5afdb1c0ef6c805
kartikey1188 <kartikeysingh428@gmail.com>
2025-05-05 07:25:57 +0530

    GORM Models Rectified

--- internal/models/setup.go
+++ internal/models/setup.go
@@ -4,10 +4,11 @@ import (
 	"github.com/kartikey1188/build-in-progress_01/internal/config"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func SetupGorm(cfg *config.Config) (*gorm.DB, error) {
-	db, err := gorm.Open(postgres.Open(cfg.StoragePath), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(cfg.StoragePath), &gorm.Config{Logger: logger.Default.LogMode(logger.Silent)})
 	if err != nil {
 		return nil, err
 	}
